2b102f5085a6e6b04c8828ca57c49e5bfaae3c08
jonathangus <jonathan.gustafsson@wunderkraut.com>
2017-02-09 00:25:34 +0100

    cors stuff

--- server.js
+++ server.js
@@ -7,11 +7,13 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
 var port = process.env.PORT || 3000;        // set our port
 console.log('GETTING HERE')
